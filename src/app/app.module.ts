f73da74808ab9c339760268a20ae554665b9fac1
Jaydutt Shukla <jaydutt.shukla@letsnurture.com>
2020-06-09 20:24:25 +0530

    Components Part 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,9 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MenuComponent } from './menu/menu.component';
 import { MatListModule } from '@angular/material/list';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -18,7 +21,10 @@ import { MatListModule } from '@angular/material/list';
     BrowserAnimationsModule,
     MatToolbarModule,
     FlexLayoutModule,
-    MatListModule
+    MatListModule,
+    MatGridListModule,
+    MatCardModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,17 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Dish } from '../shared/dish';
 
-@Component({
-  selector: 'app-menu',
-  templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.scss']
-})
-export class MenuComponent implements OnInit {
-
-  constructor() { }
-  
-  dishes: Dish[] = [
-    {
+const DISHES: Dish[] = [
+{
       id: '0',
       name: 'Uthappizza',
       image: '/assets/images/uthappizza.png',
@@ -52,8 +43,21 @@ export class MenuComponent implements OnInit {
       price: '2.99',
       description: 'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms'
     }
-   ];
+  ];
 
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.scss']
+})
+export class MenuComponent implements OnInit {
+
+  constructor() { }
+
+  dishes = DISHES;
+
+  selectedDish = DISHES[0];
+  
   ngOnInit() {
   }
 

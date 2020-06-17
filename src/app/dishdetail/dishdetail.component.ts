f6aa269fb56ac5e6ebdbb3d234a3b34f3d1490a3
Jaydutt Shukla <jaydutt.shukla@letsnurture.com>
2020-06-17 11:01:29 +0530

    Animations

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -4,25 +4,37 @@ import { DishService } from '../services/dish.service';
 import { Params, ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 import { switchMap } from 'rxjs/operators';
-
+import { visibility } from '../app.animation';
 
 @Component({
   selector: 'app-dishdetail',
   templateUrl: './dishdetail.component.html',
-  styleUrls: ['./dishdetail.component.scss']
+  styleUrls: ['./dishdetail.component.scss'],
+  animations: [
+    visibility()
+  ]
 })
 export class DishdetailComponent implements OnInit {
     dish: Dish;
     dishIds: string[];
     prev: string;
     next: string;
+    visibility = 'shown';
+
     constructor(private dishService: DishService,
       private route: ActivatedRoute,
       private location: Location) { }  
     ngOnInit() {
       this.dishService.getDishIds().subscribe(dishIds => this.dishIds = dishIds);
-      this.route.params.pipe(switchMap((params: Params) => this.dishService.getDish(params['id'])))
-      .subscribe(dish => { this.dish = dish; this.setPrevNext(dish.id); });
+
+      this.route.params.pipe(switchMap((params: Params) => {this.visibility = 'hidden'; return this.dishService.getDish(params['id'])}))
+      .subscribe(dish => { this.dish = dish; this.setPrevNext(dish.id);this.visibility = 'shown'; });
+      /*
+      Animation  
+      this.route.params.pipe(switchMap((params: Params) => { this.visibility = 'hidden'; return this.dishservice.getDish(+params['id']); }))
+      .subscribe(dish => { this.dish = dish; this.dishcopy = dish; this.setPrevNext(dish.id); this.visibility = 'shown'; },
+      errmess => this.errMess = <any>errmess); 
+      */
     }
 
     setPrevNext(dishId: string) {
